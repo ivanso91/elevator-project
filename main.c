9ef85db4c3d898ade6cd81f5aff92d923d8d1242
Ivan <ivanso@stud.ntnu.no>
2018-02-21 15:19:09 +0100

    Debug in main

--- main.c
+++ main.c
@@ -52,11 +52,11 @@ int main() {
 			// Determine new direction for elevator - allways if elevator is in floor
 			newDir = determineDirection(reqArr, maxReq, currentFloor, currentDir);
 			// Set elevator direction if changed
-			if (newDir != currentDir) {
+			//if (newDir != currentDir) {
 				currentDir = elev_set_motor_direction(newDir);
                 // Avoid stopping again at the same floor after elevator start moving
 				// timer(1, reqArr, maxReq, currentDir); 
-			}
+			//}
 		}
 
         
